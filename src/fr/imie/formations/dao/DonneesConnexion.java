5d2cbb9b7a0ce368632103de95600b7e9761b979
takiguchi <takiguchi@Packardbian>
2016-02-18 21:11:44 +0100

    Gitignore

--- src/fr/imie/formations/dao/DonneesConnexion.java
+++ src/fr/imie/formations/dao/DonneesConnexion.java
@@ -1,9 +1,9 @@
 package fr.imie.formations.dao;
 
 public class DonneesConnexion {
-    public static final String HOST  = "ADRESSE";
-    public static final String PORT  = "PORT";
-    public static final String BASE  = "NOM BDD";
-    public static final String LOGIN = "USER";
-    public static final String PSWD  = "PASSWORD";
+    public static final String HOST  = "localhost";
+    public static final String PORT  = "5432";
+    public static final String BASE  = "cdpn07_tp_jdbc";
+    public static final String LOGIN = "cdpn07";
+    public static final String PSWD  = "passe";
 }
