dda85daebd2bc2410ffea3d0c0d9188ef2408243
Takiguchi <takiguchi@TakiDebian>
2016-02-18 16:08:59 +0100

    Ajout des donénes de connexion

---
+++ src/fr/imie/formations/dao/DonneesConnexion.java
@@ -0,0 +1,9 @@
+package fr.imie.formations.dao;
+
+public class DonneesConnexion {
+    public static final String HOST  = "ADRESSE";
+    public static final String PORT  = "PORT";
+    public static final String BASE  = "NOM BDD";
+    public static final String LOGIN = "USER";
+    public static final String PSWD  = "PASSWORD";
+}
